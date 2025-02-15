7fc2c6d90a57acbb440e9ea0dbfa9781142ad43b
regiszaum <regisf97@gmail.com>
2025-02-15 16:33:17 -0300

    📝 chore: initial project setup with Vue 3, Vuex, Stylus, and FontAwesome

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Adicionar ícones ao FontAwesome
+library.add(faPlus);
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.use(router)
+app.use(store);
+app.mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+  actions: {
+    increment({ commit }) {
+      commit('increment');
+    },
+  },
+});
+
+export default store;
